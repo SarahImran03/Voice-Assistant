618874ebd39da99aa685d05cf64fd4d89e46103b
Sarah Imran <166857310+SpaceClover@users.noreply.github.com>
2024-07-21 16:18:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from gtts import gTTS
 import speech_recognition as sr
 import winsound
 from datetime import datetime
+from AppOpener import open, close
+
 
 # First listen to the given command and convert to text
 def listen():
-    recognizer = sr.Recognizer
+    recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
         print("Listening...")
@@ -17,7 +19,7 @@ def listen():
         audio = recognizer.listen(source)
 
     try:
-        command = recognizer.recognize_google_cloud(audio)
+        command = recognizer.recognize_google(audio)
         print("I heard: ", command)
         return command.lower()
     except sr.UnknownValueError:
@@ -39,11 +41,13 @@ def respond(answer):
 # Function to carry out specific tasks.
 task_list = []
 listening = False
+screenshots_folder = os.path.join(os.path.expanduser("~"), "Screenshots")
+os.makedirs(screenshots_folder, exist_ok=True)
 
 def main():
     global task_list
     global listening
-    VA_name = "Space Crab"
+    VA_name = "clover"
 
     while True:
         command = listen()
@@ -63,21 +67,27 @@ def main():
                 now = datetime.now()
                 current_time = now.strftime("%H:%M:%S")
                 respond("The time is" + current_time)
-            elif "open chrome" in command:
+            elif "open Chrome" in command:
                 respond("Opening Chrome")
                 webbrowser.open('https://www.google.com/')
             elif "open whatsapp" in command:
                 respond("Opening WhatsApp")
-                os.system("WhatsApp")
+                open("whatsapp")
+            elif "close whatsapp" in command:
+                close("whatsapp")
+                respond("Whatsapp closed")
             elif "take a screenshot" in command:
+                global screenshots_folder
                 pyautogui.screenshot("screenshot.png")
+                screenshot_path = os.path.join(screenshots_folder, "screenshot.png")
+                pyautogui.screenshot("screenshot.png").save(screenshot_path)
                 respond("I have taken a screenshot")
-            elif "close" in command:
+            elif "exit" in command:
                 respond("Goodbye")
                 break
             else:
                 respond("Sorry I can't handle that command")
+# more tasks can be added
 
 if __name__ == '__main__':
     main()
-
